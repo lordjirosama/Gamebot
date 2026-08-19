2ab54631c67751ec75a9d1e2bf04ef1e2ea1b64b
lordjirosama <lordjiro615@gmail.com>
2026-08-19 05:41:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = os.getenv("BOT_TOKEN", "").strip()
+TOKEN = os.getenv("BOT_TOKEN", "8997991259:AAHcURkl4XSwEpfFPeyIEPSLjl0WnkJyke0").strip()
 BOT_NAME = "Solurix"
 BRAND_CHANNEL = "https://t.me/Solurix_bots"
 DB_PATH = os.getenv("DB_PATH", "data/solurix.db")
